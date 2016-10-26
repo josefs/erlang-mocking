4a0e7442443c5e2f03b470c75d2f1657fe143be9
Josef Svenningsson <josef.svenningsson@gmail.com>
2016-10-26 17:16:56 +0200

    Added protocol.erl
    A test to combine mocking and proper

---
+++ protocol.erl
@@ -0,0 +1,34 @@
+-module(protocol).
+-author(josef).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-export([send/1,rec/0]).
+
+send(Msg) ->
+    Parts = lists:split(Msg),
+    lists:foreach(fun (L) ->
+			  lower:send(L)
+		  end
+		 ,Parts).
+
+rec() ->
+    Msg = lower:rec()
+    lists:concat(Msg).
+
+protocol_prop() ->
+    FORALL(Msg,gen_msg(),
+          begin
+	      put(mock,[]).
+	      meck:expect(lower,send,fun (L) ->
+					     Apa = get(mock),
+					     put(mock,[L|Apa])
+				     end),
+	      meck:expect(lower,rec, fun () ->
+					     get(mock)
+				     end),
+	      send(Msg),
+              erase(mock),
+	      Msg == rec() andalso
+		  meck:verify_expects()
+	  end).
